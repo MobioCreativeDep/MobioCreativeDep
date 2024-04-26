cb20fd115f374fa0431eb23052705da98a2498fc
MobioCreativeDep <166125355+MobioCreativeDep@users.noreply.github.com>
2024-04-26 17:29:51 +0400

    sada

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var option = document.querySelectorAll('.button-ch');
     var frstsc = document.querySelectorAll('.onestsc');
     var scndsc = document.querySelectorAll('.twostsc');
+    var imgone = document.querySelector('.kitchen-over');
     const delay = 100;
 
     function addAppearClass(elements, offset, clas) {
@@ -11,6 +12,11 @@ document.addEventListener('DOMContentLoaded', function() {
             setTimeout(function() {
                 element.classList.add(clas);
             }, delay * i + offset);
+
+            setTimeout(function() {
+                element.classList.remove(clas);
+                element.classList.add('fullop');                
+            }, 1000 + (delay + 1    ) * i + offset);
 
         });
     }
@@ -28,6 +34,10 @@ document.addEventListener('DOMContentLoaded', function() {
                     select.classList.remove('press');
                 }, 1000);
 
+                var style_img = select.getAttribute('data-image');
+                imgone.src = style_img;
+
+                console.log(style_img);
             });
         }, 1000);
     })
