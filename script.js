033baa5a31cd218320354502776f7c478558e731
MobioCreativeDep <166125355+MobioCreativeDep@users.noreply.github.com>
2024-04-26 14:18:05 +0400

    sa

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 document.addEventListener('DOMContentLoaded', function() {
     var option = document.querySelectorAll('.button-ch');
-    var const = document.getElementsByClassName('press').animation-duration;
+    var ch_text = document.querySelectorAll('.ch-text');
+
+
+    function addMultipleClass(elements, offset, class) {
+        elements.forEach(function(element, i) {
+            setTimeout(function() {
+                element.classList.add(class);
+            }, delay * (length - i) + offset);
+        });
+    }
+
+     var apgroup = option[0,1,2] + ch_text[0];
+
+    addMultipeClass(option, 0, 'appear');
     
+
     option.forEach(function(select) {
+
+       
+
+
        setTimeout(function() { 
             select.addEventListener('click', function() {
                 
                 select.classList.add('press');
 
+                setTimeout(function() {
+                    select.classList.remove('press');
+                }, 1000);
+
             });
-        }, 0);
+        }, 1000);
     })
-});
+}); 
