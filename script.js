b4c1c5337370307418e5edf4e195d7f1a00f3498
MobioCreativeDep <166125355+MobioCreativeDep@users.noreply.github.com>
2024-05-22 13:38:13 +0400

    1st playable

--- script.js
+++ script.js
@@ -1,168 +1,83 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var option1 = document.querySelectorAll('.onestsc');
-    var option2 = document.querySelectorAll('.twostsc');
-    var anim1 = document.querySelectorAll('.angrp1');
-    var anim2 = document.querySelectorAll('.angrp2');
-    var anim3 = document.querySelectorAll('.angrp3');
-    var imgone = document.querySelector('.kitchen-over');
-    var imgtwo = document.querySelector('.table-over');
-    var ktLine = document.querySelector('.kitchen-line');
-    var tbLine = document.querySelector('.table-line');
-    var divone = document.querySelector('.first_ch');
-    var divtwo = document.querySelector('.second_ch');
-    var pack = document.querySelector('.packshot');
-    const delay = 300;
+    var option = document.querySelectorAll('.option');
+    var text = document.querySelector('.textblock');
+    var hero = document.querySelector('.picture');
+    var rect = document.querySelectorAll('.rect');
+    var image = document.getElementById('heroimg');
+    var final_text = document.querySelector('.packtext');
+    var toys = document.querySelectorAll('.toy');
+    var phone = document.querySelector('.phone')
+    var mainrm = document.getElementById("toRemove");
+    var delay = 50;
+    var scnd_offset = 1000; 
+    var lenght = option.length;
 
-    function addAppearaddClasss(elements, rmOffset, addClass, rmClass) {
-
+    function addDisappearClass(elements, offset) {
         elements.forEach(function(element, i) {
-
             setTimeout(function() {
-                element.classList.add(addClass);
-            }, delay * i);
-
-            setTimeout(function() {
-                element.classList.remove(rmClass);
-                element.classList.add('fullop');                
-            }, rmOffset + (delay + 1) * i);
-
+                element.classList.add('disappear');
+            }, delay * (length - i) + offset);
         });
     }
-
-
-    function addPress(element) {
-
-        element.classList.add('press');
-
-                setTimeout(function() {
-                    element.classList.remove('press');
-                }, 1000);
-
-    }
-
-
-    addAppearaddClasss(anim1, 1000, 'appear', 'appear');
-
-
-    console.log(option1);
-           
-    option1.forEach(function(select) {
+    
+    option.forEach(function(select) {
 
         setTimeout(function() { 
-
             select.addEventListener('click', function() {
 
-                console.log(select);
+                select.classList.add('pressAnim');
                 
-                select.classList.add('press');
-                ktLine.remove();
-                tbLine.classList.remove('noop');
-                tbLine.classList.add('blinking');
+                addDisappearClass(option, 0);
 
-                setTimeout(function() {
-                    select.classList.remove('press');
-                }, 1000);
+                option.forEach(function(other) {
 
-                option1.forEach(function(other, i) {
-
+                    // Проверяем, не является ли текущий элемент выбранным
                     if (other !== select) {
-
-                        other.classList.remove('fullop');    
+                        
+                        var child = other.querySelectorAll('.style, .digit');
+                        // Присваиваем класс всем элементам, кроме выбранного
                         other.classList.add('deselect');
 
+                        child.forEach(function(whiteChild) {
+                            whiteChild.classList.add('white-txt')
+                        });
                     }
-
                 });
+                
+                addDisappearClass(toys, 200);
 
                 setTimeout(function() {
-                    select.classList.remove('press');
-                }, 1000);
-
-                anim1.forEach(function(element, i) {
-
-                    setTimeout(function() {
-                        element.classList.add('disappear');
-                    },1000 + delay * i);
-        
-                });
-
-                setTimeout(function() {
-                    divone.remove();
-                },3000 );
-
-                setTimeout(function() {
-
-                    divtwo.classList.remove('noop');
-                    addAppearaddClasss(anim2, 2000, 'appear', 'appear');
-                    
-                }, 2000);
+                    text.classList.add('disappear');
+                }, delay * (lenght + 1));
 
                 var style_img = select.getAttribute('data-image');
-                imgone.src = style_img;
+                image.src = style_img;
 
-                console.log(style_img);
+                setTimeout(function () {
+                      mainrm.remove();
+                  }, delay * (lenght + 1) + scnd_offset);
 
+                setTimeout(function() {
+                    final_text.classList.add('second-appear');
+                }, delay * (lenght + 1) + scnd_offset);
+
+                setTimeout(function() {
+                    hero.classList.add('second-appear');
+                }, delay * (lenght + 1) + scnd_offset);
+                
+                rect.forEach(function(rects, k){
+
+                    setTimeout(function() {
+                        rects.classList.add('second-appear');
+                    }, delay * (lenght - k) + scnd_offset + 100);
+                });
+
+                setTimeout(function() {
+                    phone.classList.add('second-appear');
+                }, delay * (lenght + 1)+1400);
+               
+                console.log(lenght);
             });
         }, 1000);
     })
-
-    option2.forEach(function(select) {
-
-        setTimeout(function() { 
-
-            select.addEventListener('click', function() {
-
-                console.log(select);
-                
-                addPress(select);
-                tbLine.remove();
-
-                option2.forEach(function(other, i) {
-
-                    if (other !== select ) {
-
-                        other.classList.remove('fullop');    
-                        other.classList.add('deselect');
-
-                    }
-
-                });
-
-                anim2.forEach(function(element, i) {
-
-                    setTimeout(function() {
-                        element.classList.add('disappear');
-                    },1000 + delay * i);
-        
-                });
-
-                imgtwo.classList.add('appear');
-
-                setTimeout(function() {
-                    divtwo.remove();
-                },3000 );
-
-                console.log(anim3);
-
-                setTimeout(function() {
-
-                    pack.classList.remove('noop');
-                    anim3.forEach(function(element, i) {
-
-                        setTimeout(function() {
-                            element.classList.add('packappear');
-                        }, delay / 6 * (5 - i));
-                        
-                    });
-                    
-                }, 2000);
-
-                var style_img = select.getAttribute('data-image');
-                imgtwo.src = style_img;
-
-                console.log(style_img);
-
-            });
-        },200);
-    })
-}); 
+});
